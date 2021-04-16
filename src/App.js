16bac3f28a20b7ee1135b374feedeaed7594a69a
mh-saeed <saeedkhan111244@gmail.com>
2021-04-16 17:39:47 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import './App.css';
 import Student from './students';
 
 function App() {
-  let x=1;
+  let x = 1;
   return (
     <div className="App" >
       <Student firstStudentName={"Muhammad Saeed"} dish={"Beef Biryani"} age={"21"} stdNo={x++} />
-<hr/>
+      
+      <hr />
+
       <Student firstStudentName={"Muhammad Hassan"} dish={"Chicken Karahi"} age={"22"} stdNo={x++} />
+
+      <hr />
+
+      <Student firstStudentName={"Muhammad Moiz"} dish={"Chicken Pilao"} age={"25"} stdNo={x++} />
 
     </div>
 
